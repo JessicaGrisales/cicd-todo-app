d298dd85b03dc51e32a9ff239f1e2d99a959aa42
JessicaGrisales <pi43jro@eduvaud.ch>
2025-12-11 14:42:22 +0100

    feat(model):Modification model user

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,39 @@
-module.exports = (sequelize, DataTypes) => {
+import mongoose from 'mongoose'; // Nouveau
+const { Schema, model } = mongoose;
+
+const User =new Schema({
+  email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    address: {
+      type: DataTypes.STRING
+    },
+    zip: {
+      type: DataTypes.INTEGER
+    },
+    location: {
+      type: DataTypes.STRING
+    }
+  });
+  return User;
+
+const UserSchema = model('user', User)
+export default UserSchema;
+
+
+/*module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     email: {
       type: DataTypes.STRING,
@@ -26,4 +61,5 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
   return User;
-};
+};*/
+
