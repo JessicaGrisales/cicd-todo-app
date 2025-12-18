c9fae483ce395db9434820702389f79d534e150b
JessicaGrisales <pi43jro@eduvaud.ch>
2025-12-18 14:16:06 +0100

    feat(models):Adaptation model index vers mongoose

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,13 +1,9 @@
-const { DataTypes } = require('sequelize');
-
-const UserModel = require('./user.model');
-const TodoModel = require('./todo.model');
-
-function initModels(sequelize) {
-  const User = UserModel(sequelize, DataTypes);
-  const Todo = TodoModel(sequelize, DataTypes);
-
-  return { User, Todo };
-}
-
 module.exports = { initModels };
+
+const User = require('./user.model');
+const Todo = require('./todo.model');
+
+module.exports = {
+  User,
+  Todo
+};
