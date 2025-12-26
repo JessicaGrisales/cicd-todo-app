ce3f3cd49e8a2d3818f0dcb82823fc431332cf14
JessicaGrisales <pi43jro@eduvaud.ch>
2025-12-26 11:14:11 +0100

    chore(modèle todo):Adaptation à mongo création du schéma + modèle

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -1,29 +1,32 @@
 const mongoose = require('mongoose');
 
-const todoSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true
+const todoSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
   },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-});
+);
 
-//todoSchema.index({ text: 'text' });
+//const Todo = model('Todo', todoSchema);
 
-const Todo = model('Todo', todoSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 
-export default Todo;
+module.exports = Todo;
