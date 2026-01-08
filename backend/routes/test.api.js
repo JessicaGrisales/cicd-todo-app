a3496a6c6a5bef6dbbc129a49dc237d32cb9774c
JessicaGrisales <pi43jro@eduvaud.ch>
2026-01-08 14:34:08 +0100

    feat(test.api.js):Changement de sequelize.sync()  par clearDatabase()

--- backend/routes/test.api.js
+++ backend/routes/test.api.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { sequelize } = require('../config/database');
+//const { sequelize } = require('../config/database');
+const { clearDatabase } = require('../config/database');
 
 // POST /test/reset – drop & recreate schema
 // Used to reset the database between tests
 router.post('/reset', async (req, res) => {
   try {
-    await sequelize.sync({ force: true });
+    await clearDatabase();
     return res.status(200).json({ ok: true });
   } catch (err) {
     console.error('TEST RESET failed:', err);
